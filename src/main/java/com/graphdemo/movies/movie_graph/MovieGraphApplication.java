91aa93a211b13ed117570ebafc46b452c8ce760b
Akash2918 <akashmalvekar2918@yahoo.com>
2025-09-04 22:14:43 +0530

    Initial Commit

---
+++ src/main/java/com/graphdemo/movies/movie_graph/MovieGraphApplication.java
@@ -0,0 +1,13 @@
+package com.graphdemo.movies.movie_graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graphdemo/movies/movie_graph/MovieGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphdemo.movies.movie_graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
